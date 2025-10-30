4b88b87c6b4d824a2d106d1002cb3a9519d8a2e8
suman das <sumankrdas1999@gmail.com>
2025-10-30 19:00:40 +0530

    dynamic new page

--- app/services/business_continuity/page.tsx
+++ app/services/business_continuity/page.tsx
@@ -1,10 +1,93 @@
+// "use client";
+
+// import React from "react";
+// import Image from "next/image";
+// import ServiceContactSection from "@/component/ServiceContact";
+
+// const BusinessContinuityPlan: React.FC = () => {
+//   return (
+//     <>
+//       {/* Page Title Section */}
+//       <section
+//         className="page-title about_box"
+//         style={{
+//           backgroundImage:
+//             "url(https://amfics.io/images/background/about_us.jpg)",
+//         }}>
+//         <div className="auto-container about_title">
+//           <h1>Business Continuity Plan</h1>
+//           <span className="title_divider"></span>
+//         </div>
+//       </section>
+//       {/* Case Study Section */}
+//       <section className="case-study-section zero_ser soc_item business_com">
+//         <div className="auto-container">
+//           <div className="sec-title text-center">
+//             <h3>Business Continuity Plan</h3>
+//             <div className="text soc_text cloud">
+//               Amfics offers a Business Continuity Plan and Disaster Recovery
+//               Plan review using <strong>ISO 22301</strong>. This standard
+//               provides a framework for managing and ensuring the continuity of
+//               critical business operations in the face of disruptions, such as
+//               natural disasters, cyber-attacks, and other unexpected events.
+//               <br />
+//               <br />
+//               A holistic assessment of the Business Continuity Plan and Disaster
+//               Recovery Plan can help to identify potential vulnerabilities and
+//               areas for improvement. This may include reviewing the existing
+//               plans, policies, and procedures, as well as conducting risk
+//               assessments to identify potential threats and the impact they
+//               could have on business operations.
+//               <br />
+//               <br />
+//               This can help to minimize the impact of unexpected events on
+//               business operations, reduce downtime, and ultimately improve the
+//               organization&apos;s overall resilience to cyber threats.
+//             </div>
+
+//             <div className="col-md-12">
+//               <div className="business_box">
+//                 <Image
+//                   src="https://amfics.io/images/background/business_img.jpg"
+//                   alt="Business Continuity"
+//                   width={900}
+//                   height={500}
+//                   priority
+//                 />
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </section>
+//       <ServiceContactSection />;
+//     </>
+//   );
+// };
+
+// export default BusinessContinuityPlan;
+
 "use client";
 
-import React from "react";
 import Image from "next/image";
 import ServiceContactSection from "@/component/ServiceContact";
+import { fetchPageData } from "@/app/action/page";
 
-const BusinessContinuityPlan: React.FC = () => {
+export default async function BusinessContinuityPlan() {
+  const uid = "efd1a4bf-2962-423e-902e-9f6c632255ad";
+  const data = await fetchPageData(uid);
+  if (!data?.status) {
+    return (
+      <section className="py-20 text-center">
+        <h2>Business Continuity Plan data not found.</h2>
+      </section>
+    );
+  }
+  console.log(data);
+
+  const page = data.pagedata;
+  const item = data.pageItemdataWithSubsection?.[0];
+  const subsections = item?.subsections || [];
+
   return (
     <>
       {/* Page Title Section */}
@@ -15,7 +98,7 @@ const BusinessContinuityPlan: React.FC = () => {
             "url(https://amfics.io/images/background/about_us.jpg)",
         }}>
         <div className="auto-container about_title">
-          <h1>Business Continuity Plan</h1>
+          {/* <h1>{data.title}</h1> */}
           <span className="title_divider"></span>
         </div>
       </section>
@@ -62,6 +145,4 @@ const BusinessContinuityPlan: React.FC = () => {
       <ServiceContactSection />;
     </>
   );
-};
-
-export default BusinessContinuityPlan;
+}
--- test.tsx
+++ test.tsx
@@ -1,11 +1,14 @@
 "use client";
 
-import CallToAction from "@/component/CallToActions";
+import ServiceContactSection from "@/component/ServiceContact";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
-const Services: React.FC = () => {
+const PrivacyRegulations: React.FC = () => {
   return (
     <>
+      {/* Page Title Section */}
       <section
         className="page-title about_box"
         style={{
@@ -13,170 +16,276 @@ const Services: React.FC = () => {
             "url(https://amfics.io/images/background/about_us.jpg)",
         }}>
         <div className="auto-container about_title">
-          <h1>Services</h1>
+          <h1>Privacy Regulations and Compliance</h1>
           <span className="title_divider"></span>
         </div>
       </section>
-
-      {/* globa-section */}
-      <section className="news-section global_section global_section2">
-        <div className="auto-container">
-          <div className="sec-title text-center">
-            <span className="icon">
-              <img
-                src="https://amfics.io/images/background/services_icon.png"
-                alt=""
-              />
-            </span>
-            <h3>WHAT WE OFFER?</h3>
-            <div className="text">
-              The industry spanning platforms and services ranges from banking,
-              finance, insurance, consulting, telecommunications, research &
-              development to government. We have completed overall risk
-              management in recent years while consistently reducing costs. As a
-              global leader, we have the best talent in GIAC GSEC, CEH, CBCP,
-              PMP, ITIL, CoBIT, ISO 20000, ISO 27001 security standards, and
-              certifications.
-            </div>
-            <div className="global service_image">
-              <img
-                src="https://amfics.io/images/background/zeroiz_services.jpg"
-                alt=""
-              />
-            </div>
-          </div>
-        </div>
-      </section>
-
       {/* Case Study Section */}
-      <section className="case-study-section zero_ser zero_ser2">
+      <section className="case-study-section zero_ser privacy_box">
         <div className="auto-container">
-          <div className="sec-title text-center sec-title3">
-            <h3>ZEROIZ SERVICES</h3>
+          <div className="sec-title text-center privacy_box_text">
+            <h3>Privacy Regulations and Compliance</h3>
             <div className="text">
-              The industry spanning platforms and services ranges from banking,
-              finance, insurance, consulting, telecommunications, research &
-              development to government. We have completed overall risk
-              management in recent years while consistently reducing costs. As a
-              global leader, we have the best talent in GIAC GSEC, CEH, CBCP,
-              PMP, ITIL, CoBIT, ISO 20000, ISO 27001 security standards, and
-              certifications.
+              Privacy regulations are laws and policies that aim to protect the
+              privacy of individuals and their personal information. These
+              regulations establish rules and requirements that organizations
+              must follow in order to collect, use, store, and share personal
+              information. Compliance with privacy regulations is essential for
+              organizations that handle personal information to avoid legal
+              consequences, reputation damage, and loss of consumer trust.
             </div>
           </div>
 
+          {/* Row 1 */}
           <div className="row">
-            {/* Case Block 1 */}
+            {/* Block 1 */}
             <div className="case-block col-lg-4 col-md-6 col-sm-12">
               <div className="inner-box about_inner">
                 <div className="image-box">
                   <figure className="image">
-                    <a href="#">
-                      <img
-                        src="https://amfics.io/images/resource/cloud_security.jpg"
-                        alt=""
+                    <Link href="#">
+                      <Image
+                        src="https://amfics.io/images/resource/privacy_image.jpg"
+                        alt="Privacy Awareness"
+                        width={500}
+                        height={300}
                       />
-                    </a>
+                    </Link>
                   </figure>
                   <span className="icon_img">
-                    <img
-                      src="https://amfics.io/images/icons/cloud_logo.png"
-                      alt=""
+                    <Image
+                      src="https://amfics.io/images/icons/privacy_icon.png"
+                      alt="Privacy Icon"
+                      width={60}
+                      height={60}
                     />
                   </span>
                 </div>
                 <div className="lower-content">
                   <h4>
-                    <a href="#">Cloud Security</a>
+                    <Link href="#">AWARENESS & COMMUNICATION</Link>
                   </h4>
-                  <div className="text">
-                    Cloud security refers to the technologies, policies,
-                    controls, and services that protect cloud data,
-                    applications, and infrastructure from threats.
+                  <div className="text privacy_text">
+                    Amfics can develop an Information Security Policy so that
+                    all employees understand privacy regulations as well as the
+                    proper communication channels to ensure proper data
+                    handling.
                   </div>
                   <div className="btn-box">
-                    <a
-                      href="https://zeroiz.tezcommerce.com/services/cloud"
-                      className="theme-btn icon-btn-one">
-                      <span>View Services</span>
-                    </a>
+                    <Link href="#" className="theme-btn icon-btn-one">
+                      <span>View Privacy</span>
+                    </Link>
                   </div>
                 </div>
               </div>
             </div>
 
-            {/* Case Block 2 */}
+            {/* Block 2 */}
             <div className="case-block col-lg-4 col-md-6 col-sm-12">
               <div className="inner-box about_inner">
                 <div className="image-box">
                   <figure className="image">
-                    <a href="#">
-                      <img
-                        src="https://amfics.io/images/resource/simulate.jpg"
-                        alt=""
+                    <Link href="#">
+                      <Image
+                        src="https://amfics.io/images/resource/privacy_image2.jpg"
+                        alt="Audit and Analysis"
+                        width={500}
+                        height={300}
                       />
-                    </a>
+                    </Link>
                   </figure>
                   <span className="icon_img">
-                    <img
-                      src="https://amfics.io/images/icons/simulate_icon.png"
-                      alt=""
+                    <Image
+                      src="https://amfics.io/images/icons/privacy_icon2.png"
+                      alt="Privacy Icon 2"
+                      width={60}
+                      height={60}
                     />
                   </span>
                 </div>
                 <div className="lower-content">
                   <h4>
-                    <a href="#">Simulate Real-world Attack</a>
+                    <Link href="#">AUDIT & ANALYSIS OF PERSONAL DATA</Link>
                   </h4>
-                  <div className="text">
-                    Attack your security posture using adversarial tactics from
-                    the MITRE ATT&CK framework. Simulate the latest attacks
-                    safely on your unique environment without exposing data.
+                  <div className="text privacy_text">
+                    Amfics can help you analyse and track sensitive data through
+                    storage and processing, as well as determine data ownership
+                    roles.
                   </div>
                   <div className="btn-box">
-                    <a
-                      href="https://zeroiz.tezcommerce.com/services/simulate"
-                      className="theme-btn icon-btn-one">
-                      <span>View Services</span>
-                    </a>
+                    <Link href="#" className="theme-btn icon-btn-one">
+                      <span>View Privacy</span>
+                    </Link>
                   </div>
                 </div>
               </div>
             </div>
 
-            {/* Case Block 3 */}
+            {/* Block 3 */}
             <div className="case-block col-lg-4 col-md-6 col-sm-12">
               <div className="inner-box about_inner">
                 <div className="image-box">
                   <figure className="image">
-                    <a href="#">
-                      <img
-                        src="https://amfics.io/images/resource/ot_security.jpg"
-                        alt=""
+                    <Link href="#">
+                      <Image
+                        src="https://amfics.io/images/resource/privacy_image3.jpg"
+                        alt="Protect Private Data"
+                        width={500}
+                        height={300}
                       />
-                    </a>
+                    </Link>
                   </figure>
                   <span className="icon_img">
-                    <img
-                      src="https://amfics.io/images/icons/ot_icon.png"
-                      alt=""
+                    <Image
+                      src="https://amfics.io/images/icons/privacy_icon3.png"
+                      alt="Privacy Icon 3"
+                      width={60}
+                      height={60}
                     />
                   </span>
                 </div>
                 <div className="lower-content">
                   <h4>
-                    <a href="#">OT/IOT Security</a>
+                    <Link href="#">PROTECT PRIVATE DATA</Link>
                   </h4>
-                  <div className="text">
-                    Siemens Cybersecurity for Industry solutions for plant
-                    automation - more information! Protecting productivity with
-                    Plant Security Services by Siemens for your security!
+                  <div className="text privacy_text privacy_text2">
+                    We can help develop an IT strategy to implement data
+                    protections, backups and a rescue plan to guarantee business
+                    continuity in case of a data breach.
                   </div>
                   <div className="btn-box">
-                    <a
-                      href="https://zeroiz.tezcommerce.com/services/ot_sceurity"
-                      className="theme-btn icon-btn-one">
-                      <span>View Services</span>
-                    </a>
+                    <Link href="#" className="theme-btn icon-btn-one">
+                      <span>View Privacy</span>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Row 2 */}
+          <div className="row privacy_item">
+            {/* Block 4 */}
+            <div className="case-block col-lg-4 col-md-6 col-sm-12">
+              <div className="inner-box about_inner">
+                <div className="image-box">
+                  <figure className="image">
+                    <Link href="#">
+                      <Image
+                        src="https://amfics.io/images/resource/privacy_image4.jpg"
+                        alt="Access Rights"
+                        width={500}
+                        height={300}
+                      />
+                    </Link>
+                  </figure>
+                  <span className="icon_img">
+                    <Image
+                      src="https://amfics.io/images/icons/privacy_icon4.png"
+                      alt="Privacy Icon 4"
+                      width={60}
+                      height={60}
+                    />
+                  </span>
+                </div>
+                <div className="lower-content">
+                  <h4>
+                    <Link href="#">ACCESS RIGHTS & CUSTOMER CONSENT</Link>
+                  </h4>
+                  <div className="text privacy_text">
+                    Guidance in obtaining proper and legally valid consent from
+                    your customers. Our legal partners help guarantee that data
+                    is gathered correctly and is fully compliant with privacy
+                    guidelines.
+                  </div>
+                  <div className="btn-box">
+                    <Link href="#" className="theme-btn icon-btn-one">
+                      <span>View Privacy</span>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/* Block 5 */}
+            <div className="case-block col-lg-4 col-md-6 col-sm-12">
+              <div className="inner-box about_inner">
+                <div className="image-box">
+                  <figure className="image">
+                    <Link href="#">
+                      <Image
+                        src="https://amfics.io/images/resource/privacy_image5.jpg"
+                        alt="Data Protection Officer"
+                        width={500}
+                        height={300}
+                      />
+                    </Link>
+                  </figure>
+                  <span className="icon_img">
+                    <Image
+                      src="https://amfics.io/images/icons/privacy_officer_icon.png"
+                      alt="Privacy Officer Icon"
+                      width={60}
+                      height={60}
+                    />
+                  </span>
+                </div>
+                <div className="lower-content">
+                  <h4>
+                    <Link href="#">APPOINT A DATA PROTECTION OFFICER</Link>
+                  </h4>
+                  <div className="text privacy_text">
+                    Many firms experience difficulty with creating this new
+                    mandated role. Amfics can help select the best internal
+                    resources to act as DPO as well as assist in building a
+                    supporting team.
+                  </div>
+                  <div className="btn-box">
+                    <Link href="#" className="theme-btn icon-btn-one">
+                      <span>View Privacy</span>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/* Block 6 */}
+            <div className="case-block col-lg-4 col-md-6 col-sm-12">
+              <div className="inner-box about_inner">
+                <div className="image-box">
+                  <figure className="image">
+                    <Link href="#">
+                      <Image
+                        src="https://amfics.io/images/resource/privacy_image6.jpg"
+                        alt="Impact Assessment"
+                        width={500}
+                        height={300}
+                      />
+                    </Link>
+                  </figure>
+                  <span className="icon_img">
+                    <Image
+                      src="https://amfics.io/images/icons/privacy_impact_icon.png"
+                      alt="Privacy Impact Icon"
+                      width={60}
+                      height={60}
+                    />
+                  </span>
+                </div>
+                <div className="lower-content">
+                  <h4>
+                    <Link href="#">IMPACT ASSESSMENTS</Link>
+                  </h4>
+                  <div className="text privacy_text privacy_text2">
+                    We can carry out a data protection impact assessment, and
+                    help you with threat modeling and risk aversion. We can also
+                    assist in making this assessment part of an overall business
+                    continuity program.
+                  </div>
+                  <div className="btn-box">
+                    <Link href="#" className="theme-btn icon-btn-one">
+                      <span>View Privacy</span>
+                    </Link>
                   </div>
                 </div>
               </div>
@@ -184,9 +293,9 @@ const Services: React.FC = () => {
           </div>
         </div>
       </section>
-      <CallToAction />
+      <ServiceContactSection />;
     </>
   );
 };
 
-export default Services;
+export default PrivacyRegulations;
