b093071397dcd146f42d817c8f4b185339b2bae5
suman das <sumankrdas1999@gmail.com>
2026-01-16 17:47:14 +0530

    changes

--- component/ScrollTop.tsx
+++ component/ScrollTop.tsx
@@ -1,28 +1,3 @@
-// "use client";
-
-// import React from "react";
-// import { FaChevronUp } from "react-icons/fa";
-
-// const ScrollTop = () => {
-//   const scrollToTop = () => {
-//     window.scrollTo({
-//       top: 0,
-//       behavior: "smooth",
-//     });
-//   };
-
-//   return (
-//     <div
-//       className="scroll-to-top scroll-to-target"
-//       onClick={scrollToTop}
-//       style={{ display: "block", cursor: "pointer" }}>
-//       {/* <span className="flaticon-arrow-up"></span> */}
-//       <FaChevronUp />
-//     </div>
-//   );
-// };
-
-// export default ScrollTop;
 "use client";
 
 import React, { useEffect, useState } from "react";
@@ -47,8 +22,7 @@ const ScrollTop = () => {
     });
   };
 
-  if (!show) return null; // 👈 top এ থাকলে hide
-
+  if (!show) return null;
   return (
     <div
       className="scroll-to-top scroll-to-target"
